dc872a59a7db00165a250b7768fd784f2660bbab
Dave Francis Rotaquio <mail@daverotaquio.com>
2025-07-15 09:15:07 +0800

    TECH-45262: Remove Autofixture

--- samples/MyCRM.Lodgement.Sample.Tests/Services/Client/LodgementClientTests.cs
+++ samples/MyCRM.Lodgement.Sample.Tests/Services/Client/LodgementClientTests.cs
@@ -4,7 +4,6 @@ using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using AutoFixture;
 using FluentAssertions;
 using Microsoft.Extensions.Options;
 using Moq;
@@ -15,93 +14,105 @@ using MyCRMAPI.Lodgement.Models;
 using Newtonsoft.Json.Linq;
 using Xunit;
 
-namespace MyCRM.Lodgement.Sample.Tests.Services.Client
+namespace MyCRM.Lodgement.Sample.Tests.Services.Client;
+
+public class LodgementClientTests
 {
-    public class LodgementClientTests
+    private class DelegatingHandlerStub : DelegatingHandler
     {
-        private class DelegatingHandlerStub : DelegatingHandler
+        private readonly Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> _handlerFunc;
+        public DelegatingHandlerStub(Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> handlerFunc) => _handlerFunc = handlerFunc;
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) => _handlerFunc(request, cancellationToken);
+    }
+
+    private const string MediaType = "application/json";
+    private readonly Mock<IHttpClientFactory> _httpClientFactoryMock;
+    private readonly Mock<IOptions<LodgementSettings>> _optionsMock;
+
+    public LodgementClientTests()
+    {
+        _httpClientFactoryMock = new Mock<IHttpClientFactory>();
+        _optionsMock = new Mock<IOptions<LodgementSettings>>();
+    }
+
+    [Fact]
+    public async Task WhenValidatingAndOkResult_ThenReturnValidationResult()
+    {
+        var package = new Package();
+
+        var validationResult = new ValidationResult
         {
-            private readonly Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> _handlerFunc;
-            public DelegatingHandlerStub(Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> handlerFunc) => _handlerFunc = handlerFunc;
-            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) => _handlerFunc(request, cancellationToken);
-        }
+            ExternalReferenceId = Guid.NewGuid().ToString(),
+            ValidationErrors = []
+        };
 
-        private const string MediaType = "application/json";
-        private readonly Mock<IHttpClientFactory> _httpClientFactoryMock;
-        private readonly Mock<IOptions<LodgementSettings>> _optionsMock;
-
-        public LodgementClientTests()
+        var settings = new LodgementSettings
         {
-            _httpClientFactoryMock = new Mock<IHttpClientFactory>();
-            _optionsMock = new Mock<IOptions<LodgementSettings>>();
-        }
+            MediaType = MediaType,
+            Url = "sample-url"
+        };
 
-        [Fact]
-        public async Task WhenValidatingAndOkResult_ThenReturnValidationResult()
+        var clientHandlerStub = new DelegatingHandlerStub((request, cancellationToken) =>
         {
-            var fixture = new Fixture();
-
-            var package = fixture.Create<Package>();
-            var validationResult = fixture.Create<ValidationResult>();
-            var settings = fixture.Build<LodgementSettings>()
-                .With(x => x.MediaType, MediaType).Create();
-
-            var clientHandlerStub = new DelegatingHandlerStub((request, cancellationToken) =>
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
             {
-                var response = new HttpResponseMessage(HttpStatusCode.OK)
-                {
-                    Content = new StringContent(JObject.FromObject(validationResult).ToString(), Encoding.UTF8, MediaType)
-                };
-                return Task.FromResult(response);
-            });
+                Content = new StringContent(JObject.FromObject(validationResult).ToString(), Encoding.UTF8, MediaType)
+            };
+            return Task.FromResult(response);
+        });
 
-            _optionsMock.Setup(x => x.Value)
-                .Returns(settings);
+        _optionsMock.Setup(x => x.Value)
+            .Returns(settings);
 
-            var client = new HttpClient(clientHandlerStub) { BaseAddress = new Uri("https://example.com/") };
+        var client = new HttpClient(clientHandlerStub) { BaseAddress = new Uri("https://example.com/") };
 
-            _httpClientFactoryMock.Setup(_ => _.CreateClient(nameof(LodgementClient)))
-                .Returns(client);
+        _httpClientFactoryMock.Setup(_ => _.CreateClient(nameof(LodgementClient)))
+            .Returns(client);
 
-            var target = new LodgementClient(_httpClientFactoryMock.Object,
-                _optionsMock.Object);
+        var target = new LodgementClient(_httpClientFactoryMock.Object,
+            _optionsMock.Object);
 
-            var result = await target.Validate(package, CancellationToken.None);
-            result.ExternalReferenceId.Should().Be(validationResult.ExternalReferenceId);
-        }
+        var result = await target.Validate(package, CancellationToken.None);
+        result.ExternalReferenceId.Should().Be(validationResult.ExternalReferenceId);
+    }
 
-        [Fact]
-        public async Task WhenSubmittingAndOkResult_ThenReturnSubmissionResult()
+    [Fact]
+    public async Task WhenSubmittingAndOkResult_ThenReturnSubmissionResult()
+    {
+        var package = new Package();
+
+        var submissionResult = new SubmissionResult
         {
-            var fixture = new Fixture();
+            ReferenceId = Guid.NewGuid().ToString()
+        };
 
-            var package = fixture.Create<Package>();
-            var submissionResult = fixture.Create<SubmissionResult>();
-            var settings = fixture.Build<LodgementSettings>()
-                .With(x => x.MediaType, MediaType).Create();
+        var settings = new LodgementSettings
+        {
+            MediaType = MediaType,
+            Url = "sample-url"
+        };
 
-            var clientHandlerStub = new DelegatingHandlerStub((request, cancellationToken) =>
+        var clientHandlerStub = new DelegatingHandlerStub((request, cancellationToken) =>
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
             {
-                var response = new HttpResponseMessage(HttpStatusCode.OK)
-                {
-                    Content = new StringContent(JObject.FromObject(submissionResult).ToString(), Encoding.UTF8, MediaType)
-                };
-                return Task.FromResult(response);
-            });
+                Content = new StringContent(JObject.FromObject(submissionResult).ToString(), Encoding.UTF8, MediaType)
+            };
+            return Task.FromResult(response);
+        });
 
-            _optionsMock.Setup(x => x.Value)
-                .Returns(settings);
+        _optionsMock.Setup(x => x.Value)
+            .Returns(settings);
 
-            var client = new HttpClient(clientHandlerStub) { BaseAddress = new Uri("https://example.com/") };
+        var client = new HttpClient(clientHandlerStub) { BaseAddress = new Uri("https://example.com/") };
 
-            _httpClientFactoryMock.Setup(_ => _.CreateClient(nameof(LodgementClient)))
-                .Returns(client);
+        _httpClientFactoryMock.Setup(_ => _.CreateClient(nameof(LodgementClient)))
+            .Returns(client);
 
-            var target = new LodgementClient(_httpClientFactoryMock.Object,
-                _optionsMock.Object);
+        var target = new LodgementClient(_httpClientFactoryMock.Object,
+            _optionsMock.Object);
 
-            var result = await target.Validate(package, CancellationToken.None);
-            result.ExternalReferenceId.Should().Be(submissionResult.ReferenceId);
-        }
+        var result = await target.Submit(package, CancellationToken.None);
+        result.Result.ReferenceId.Should().Be(submissionResult.ReferenceId);
     }
 }
